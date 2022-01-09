d9b17d70514d94bd787416825cff2b7d8410b254
sphcsphc <sphcsphc.git>
2022-01-09 14:48:09 +0900

    C와 C++ 헤더 차이

---
+++ 220103/1.cpp
@@ -0,0 +1,19 @@
+// C
+
+// #include <stdio.h>
+
+// int main()
+// {
+//   printf("Hello, C\n");
+// }
+
+// C++
+
+#include <iostream>
+#include <cstdio>
+
+int main()
+{
+    printf("Hello, C\n");
+    std::cout << "Hello, C++" << std::endl;
+}
