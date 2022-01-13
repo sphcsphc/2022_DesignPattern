052a8061cd48f4a17cb7db95b7970749c145e073
sphcsphc <sphcsphc.git>
2022-01-14 03:55:43 +0900

    Composite 패턴으로 파일 사이즈 구하기

--- 220112/33_Composite.cpp
+++ 220112/33_Composite.cpp
@@ -85,12 +85,13 @@ int main()
     Folder* fol1 = new Folder("ROOT");
     Folder* fol2 = new Folder("A");
 
-    File* f1 = new File("a.txt", 10); // 이름, 크기
+    File* f1 = new File("a.txt", 10);
     File* f2 = new File("b.txt", 20);
 
     fol2->AddItem(f1);
     fol1->AddItem(f2);
     fol1->AddItem(fol2);
+    cout << f1->GetSize() << endl;
     cout << f2->GetSize() << endl;
     cout << fol2->GetSize() << endl;
     cout << fol1->GetSize() << endl;
