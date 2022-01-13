3fd94f5ea88a6be0ed8f6cf1d83b07eca33674a1
sphcsphc <sphcsphc.git>
2022-01-14 02:07:30 +0900

    인라인 치환

--- 220112/32_함수와전략2.cpp
+++ 220112/32_함수와전략2.cpp
@@ -1,8 +1,12 @@
 // 1. 인라인 치환은 컴파일 시간 문법입니다.
 // 2. 인라인 함수라도 함수 포인터에 담아서 호출하면, 인라인 치환 될 수 없다.
 //   (함수 포인터도 결국 변수이다. 실행 시간에 변할 수 있다.)
-void foo() { }
-inline void goo() { }
+
+#include <iostream>
+using namespace std;
+
+void foo() { cout << "foo" << endl; }
+inline void goo() { cout << "goo" << endl; }
 
 int main()
 {
