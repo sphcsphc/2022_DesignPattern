23206f70fad4b9c2541f6a170f22db45aa315789
sphcsphc <sphcsphc.git>
2022-01-14 02:30:56 +0900

    함수 객체

--- 220112/32_함수와전략3.cpp
+++ 220112/32_함수와전략3.cpp
@@ -10,8 +10,7 @@ void foo() { }
 void goo() { }
 
 // 1. 함수는 자신만의 타입이 없다.
-//  > 함수의 시그니처(인자 정보, 반환 타입) 동일한 모든 함수는
-//    같은 타입입니다.
+//  > 함수의 시그니처(인자 정보, 반환 타입) 동일한 모든 함수는 같은 타입입니다.
 
 // 2. 함수 객체는 자신만의 타입이 있다.
 //  > 시그니처가 동일해도 모든 함수 객체는 다른 타입이다.
@@ -31,9 +30,12 @@ struct Minus {
 int main()
 {
     Plus plus;
+    Minus minus;
 
-    int result = plus(10, 20);
-    // plus.operator()(10, 20);
+    // int result = plus(10, 20);
+    // int result = minus(10, 20);
+
+    int result = plus.operator()(10, 20);
 
     cout << result << endl;
 }
